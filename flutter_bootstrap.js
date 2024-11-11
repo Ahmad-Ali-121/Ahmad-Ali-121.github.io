0cfebfdab9c657584b3ed821bf7217af4dc57ef6
Ahmad ali khan <ahmadali22220000@gmail.com>
2024-11-11 16:49:33 +0500

    added todo pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3332631227"
+    serviceWorkerVersion: "2393437321"
   }
 });
